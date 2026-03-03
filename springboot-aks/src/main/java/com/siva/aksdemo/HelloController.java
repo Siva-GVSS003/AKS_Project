90dbfb6b64d6092576645e8b023b3475a73af681
root <root@devops-vm.gkkscurfhhzuln5iw3oj15bwxb.xx.internal.cloudapp.net>
2026-03-03 16:38:00 +0000

    Initial commit - Spring Boot AKS project

---
+++ springboot-aks/src/main/java/com/siva/aksdemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.siva.aksdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Siva 🚀 Deployed to AKS Successfully!";
+    }
+}
