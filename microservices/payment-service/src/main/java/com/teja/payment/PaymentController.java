cf2647601d4868f3def73d6b3c6ad4106b78507a
root <root@devopsvm.zfeuvgmy4jyexeqiygu4dotknd.rx.internal.cloudapp.net>
2026-03-07 09:01:07 +0000

    adding missing files

---
+++ microservices/payment-service/src/main/java/com/teja/payment/PaymentController.java
@@ -0,0 +1,14 @@
+package com.teja.payment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    @GetMapping("/payments")
+    public String payments(){
+        return "Payment Service Running in AKS 🚀";
+    }
+
+}
