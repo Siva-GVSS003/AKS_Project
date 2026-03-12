03bb6ad1fb024e800c6ad91b8fbcbdd15cf66097
root <root@devopsvm.zfeuvgmy4jyexeqiygu4dotknd.rx.internal.cloudapp.net>
2026-03-12 17:40:33 +0000

    New code changes

--- microservices/user-service/src/main/java/com/teja/user/UserController.java
+++ microservices/user-service/src/main/java/com/teja/user/UserController.java
@@ -8,7 +8,7 @@ public class UserController {
 
     @GetMapping("/user")
     public String users(){
-        return "User Service Running in AKS (dev)🚀";
+        return "User Service Running in AKS (develop branch)🚀";
     }
 
 }
