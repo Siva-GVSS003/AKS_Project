b50eba5c79fa6ead6b5465f8ee8ba5fe4f7726c7
root <root@devopsvm.zfeuvgmy4jyexeqiygu4dotknd.rx.internal.cloudapp.net>
2026-03-13 06:53:42 +0000

    Test commit

--- microservices/user-service/src/main/java/com/teja/user/UserController.java
+++ microservices/user-service/src/main/java/com/teja/user/UserController.java
@@ -9,7 +9,7 @@ public class UserController {
     @GetMapping("/user")
     public String users(){
 
-        return "User Service Running in AKS (develop branch)🚀";
+        return "User Service Running in AKS (develop )🚀";
 
 
     }
